97f3f6d68224f5caecdfdab3c1aa888b4f34997e
Bram Delombaerde <bramdelombaerde@hotmail.com>
2023-04-04 18:32:58 +0200

    Cleanup weatherforecast

---
+++ Newsletter.Api/Controllers/TitelController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Newsletter.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TitelController : ControllerBase
+    {
+        [HttpGet(Name = "GetTitels")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
