4c3f8f47bcf038ac5b6d69f13f10c7b9153fdc99
bramd <bramd@DESKTOP-QN7INNE>
2023-04-04 17:58:17 +0200

    Add project files.

---
+++ Newsletter.Domain/Titel.cs
@@ -0,0 +1,9 @@
+﻿namespace Newsletter.Domain
+{
+    public class Titel
+    {
+        public Guid Id { get; set; }
+        public string ShortName { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Newsletter.Infrastructure/Persistence/NewsletterDatastore.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Newsletter.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newsletter.Infrastructure.Persistence;
+
+public class NewsletterDatastore : DbContext
+{
+    public NewsletterDatastore(DbContextOptions<NewsletterDatastore> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Titel> Titels { get; set; }
+}
+
+internal class DataStoreDesignTimeFactory : IDesignTimeDbContextFactory<NewsletterDatastore>
+{
+    public NewsletterDatastore CreateDbContext(string[] args)
+    {
+        return new NewsletterDatastore(
+            new DbContextOptionsBuilder<NewsletterDatastore>()
+                .UseSqlServer(
+                    "Server=localhost;Initial Catalog=sqldbnewsletter;Integrated Security=true;MultipleActiveResultSets=true")
+                .Options);
+    }
+}
