099c85d6f8896557989dca5bff32a161ff29c4dc
Bram Delombaerde <bramdelombaerde@hotmail.com>
2023-04-06 13:45:20 +0200

    Newsletter domain

---
+++ Newsletter.Domain/Newsletter.cs
@@ -0,0 +1,11 @@
+﻿namespace Newsletter.Domain
+{
+    public class Newsletter : BaseEntity
+    {
+        public Titel Titel { get; set; }
+        public int Version { get; set; }
+        public string Html { get; set; }
+        public bool IsArchived { get; set; }
+        public List<NewsletterToken> Tokens { get; set; }
+    }
+}
---
+++ Newsletter.Domain/NewsletterToken.cs
@@ -0,0 +1,6 @@
+﻿namespace Newsletter.Domain;
+public class NewsletterToken
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
