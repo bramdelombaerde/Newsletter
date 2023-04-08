a301a69d78cf05f49e35a0eb4f30686b30ffca6b
Bram Delombaerde <bramdelombaerde@hotmail.com>
2023-04-08 14:52:59 +0200

    SendNewsletterHandler

---
+++ Newsletter.Application/Newsletter/SendNewsletter.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Newsletter.Application.Shared;
+using Newsletter.Core.Repositories;
+
+namespace Newsletter.Application.Newsletter
+{
+    public record SendNewsletterCommand(Guid NewsletterId) : IRequest<IResult<SendNewsletterResponse>>;
+    public record SendNewsletterToken(string Name, string Value, string Source);
+    public record SendNewsletterResponse(Guid Id);
+
+    public class SendNewsletterHandler : IRequestHandler<SendNewsletterCommand, IResult<SendNewsletterResponse>>
+    {
+        private readonly Newsletters _newsletters;
+
+        public SendNewsletterHandler(
+            Newsletters newsletters)
+        {
+            _newsletters = newsletters;
+        }
+        public async Task<IResult<SendNewsletterResponse>> Handle(SendNewsletterCommand request, CancellationToken cancellationToken)
+        {
+            var newsletter = await _newsletters.GetById(request.NewsletterId);
+            if (newsletter == null) return Result.NotFound<SendNewsletterResponse>($"NewsletterId '{request.NewsletterId}' not found");
+
+
+
+
+
+            return Result.Success(new SendNewsletterResponse(newsletter.Id));
+
+        }
+    }
+}
