8c6a1b4058a19f42feabad820c322d0490195385
Bram Delombaerde <bramdelombaerde@hotmail.com>
2023-04-09 16:28:25 +0200

    Added Validators

---
+++ Newsletter.Api/Models/Newsletter/Validation/CreateNewsletterValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace Newsletter.Api.Models.Newsletter.Validation
+{
+    public class CreateNewsletterValidator : AbstractValidator<CreateNewsletter>
+    {
+        public CreateNewsletterValidator()
+        {
+            RuleFor(x => x.TemplateId)
+            .NotEmpty();
+
+            RuleFor(x => x.TitelId)
+            .NotEmpty();
+
+            RuleForEach(x => x.Tokens)
+                .SetValidator(new NewsletterTokenValidator());
+        }
+    }
+
+    public class NewsletterTokenValidator : AbstractValidator<NewsletterToken>
+    {
+        public NewsletterTokenValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.Source)
+                .NotNull();
+        }
+    }
+}
---
+++ Newsletter.Api/Models/NewsletterTemplate/Validation/CreateNewsletterTemplateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Newsletter.Api.Models.NewsletterTemplate.Validation
+{
+    public class CreateNewsletterTemplateValidator : AbstractValidator<CreateNewsletterTemplate>
+    {
+        public CreateNewsletterTemplateValidator()
+        {
+            RuleFor(x => x.Html)
+            .NotEmpty();
+
+            RuleFor(x => x.TemplateName)
+            .NotEmpty();
+
+            RuleFor(x => x.TitelId)
+            .NotEmpty();
+        }
+    }
+}
